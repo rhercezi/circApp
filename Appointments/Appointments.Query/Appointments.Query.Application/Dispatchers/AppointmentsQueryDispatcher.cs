139bc1c5129700ae4e5c1ce89a5031ce5f2299b3
rhercezi <roherc@gmail.com>
2024-08-13 22:06:47 +0200

    Removed unused code in appointmnt query dispatcher after reformatting

--- Appointments/Appointments.Query/Appointments.Query.Application/Dispatchers/AppointmentsQueryDispatcher.cs
+++ Appointments/Appointments.Query/Appointments.Query.Application/Dispatchers/AppointmentsQueryDispatcher.cs
@@ -8,17 +8,13 @@ namespace Appointments.Query.Application.Dispatchers
 {
     public class AppointmentsQueryDispatcher : IMessageDispatcher
     {
-        private Dictionary<Type, Type> _handlers = new();
         private readonly ILogger<AppointmentsQueryDispatcher> _logger;
         private readonly IServiceProvider _serviceProvider;
-        private readonly IHandlerService _handlerService;
         public AppointmentsQueryDispatcher(ILogger<AppointmentsQueryDispatcher> logger,
-                                           IServiceProvider serviceProvider,
-                                           IHandlerService handlerService)
+                                           IServiceProvider serviceProvider)
         {
             _logger = logger;
             _serviceProvider = serviceProvider;
-            _handlerService = handlerService;
         }
 
         public async Task<BaseResponse> DispatchAsync(BaseMessage query)
